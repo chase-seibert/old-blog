63166fae1e76cae7f3f4be1276bcc3d57d89b1ef
Chase Seibert <cseibert@nerdwallet.com>
2017-12-01 09:00:28 -0800

    new site

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+gem 'github-pages', '78'
+gem 'redcarpet', '3.3.4'
+gem 'pygments.rb', '0.6.3'
---
+++ _site/publish-website.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+jekyll build --future
+git branch -D gh-pages
+git checkout -b gh-pages
+git filter-branch --subdirectory-filter _site/ -f
+cp -R _site/* .
+git add .
+git commit -m "new site"
+git checkout master
+git push --all origin --force
+git checkout master
